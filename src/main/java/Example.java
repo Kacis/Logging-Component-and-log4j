f56229619a000ddd9d4ff08b9216cd10e688ada6
Milan Kacálek <kacis.m@gmail.com>
2013-10-06 15:58:28 +0200

    Sample Component logging.

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,3 +1,6 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Kacis
@@ -5,5 +8,14 @@
  * Time: 22:17
  */
 public class Example {
+    private static Log logger = LogFactory.getLog(Example.class);
 
+    public static void main(String[] args) {
+        logger.trace("TRACE!");
+        logger.debug("DEBUG!");
+        logger.info("INFO!");
+        logger.warn("WARM!");
+        logger.error("ERROR!");
+        logger.fatal("FATAL!");
+    }
 }
